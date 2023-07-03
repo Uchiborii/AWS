c1d77a433ec40ce3e8b86411d38bd404eeed248b
uchiborieri <zuumo219@gmail.com>
2023-07-03 21:25:00 +0900

    controller作成

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,45 @@
+class BlogsController < ApplicationController
+    def index
+      @blogs = Blog.all
+    end
+
+    def new
+      @blog = Blog.new
+    end
+
+    def create
+      @blog = current_user.blogs.build(blog_params)
+      if @blog.save
+        redirect_to blogs_path
+      else
+        render :new
+      end
+    end
+
+    def show
+      @blog = Blog.find(params[:id])
+    end
+
+    def edit
+      @blog = Blog.find(params[:id])
+    end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "編集します"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "削除しました"
+  end
+
+  def blog_params
+    params.require(:blog).permit(:photo, :title, :content, :photo_cache)
+end
+end
+
