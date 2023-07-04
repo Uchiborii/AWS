f580104db65a0df75be478554ac095b82c313a40
uchiborieri <zuumo219@gmail.com>
2023-07-04 18:18:49 +0900

    重複テーブル削除

--- db/migrate/20230703125101_add_user_id_to_blogs.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToBlogs < ActiveRecord::Migration[6.0]
-  def change
-    add_column :blogs, :user_id, :integer
-    add_index :blogs, :user_id
-  end
-end
--- db/migrate/20230703125431_remove_blog_fromuser_id.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBlogFromuserId < ActiveRecord::Migration[6.0]
-  def update
-    remove_column :user_id, :string
-  end
-end
